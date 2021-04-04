77569c9a893a0cf75e081c78f88103649856e401
vijethkgowda <vijethkgowda@gmail.com>
2021-04-04 17:34:25 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import JobList from "../components/JobList";
+
+export default function Home() {
+  return (
+    <div className="mx-6 lg:mx-48 md:mx-16 sm:mx-6 my-10">
+      <JobList />
+    </div>
+  );
+}
